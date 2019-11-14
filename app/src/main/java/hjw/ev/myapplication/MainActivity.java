890422483537d7b50f4c35fb8a0dbbad10089fcf
hujiawen <hujiawencn@163.com>
2019-11-14 17:31:48 +0800

    删除.iml文件

--- app/src/main/java/hjw/ev/myapplication/MainActivity.java
+++ app/src/main/java/hjw/ev/myapplication/MainActivity.java
@@ -1,16 +1,16 @@
-package hjw.ev.myapplication;
+package hjw.ev.myapplication
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
 
-import com.google.zxing.MultiFormatReader;
+import com.google.zxing.MultiFormatReader
 
-public class MainActivity extends AppCompatActivity {
+class MainActivity : AppCompatActivity() {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        MultiFormatReader multiFormatReader = new MultiFormatReader();
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val multiFormatReader = MultiFormatReader()
     }
 }
